f182855f41036a8c16c5f496243317c4d4dc25c5
SHRUIKAN\darre <darren.k.fernandes@gmail.com>
2025-11-29 17:40:11 -0600

    added two main functions

--- MazeSolver.pl
+++ MazeSolver.pl
@@ -5,9 +5,38 @@ cell_value(Maze, (Row, Column), CellType) :-
     nth0(Row, Maze, RowList),
     nth0(Column, RowList, CellType).
 
-% Now that I have a way to get the value of the cell
-% I can get the start value
+%now that I have a way to get the value of the cell
+%I can get the start value
 find_start(Maze, (Row, Column)):-
  once((
      nth0(Row, Maze, RowList),
      nth0(Column, RowList, s))).
+
+%move should have 4 functions, force a move up, down, left and right
+move((R, C), up, (NewR, C)) :-
+    NewR is R - 1.
+
+move((R, C), down, (NewR, C)) :-
+    NewR is R + 1.
+
+move((R, C), left, (R, NewC)) :-
+    NewC is C - 1.
+
+move((R, C), right, (R, NewC)) :-
+    NewC is C + 1.
+
+%base case, the cell has the value 'e'
+solve(Maze, CurCoords, _Visited, []) :-
+    cell_value(Maze, CurCoords, e).
+
+%recursive case, move it, then check if the value is a wall or if we already visited it, and the call the recursive case adding the current coordinates to the visited case.
+solve(Maze, CurCoords, Visited, [Direction | Rest]) :-
+    move(CurCoords, Direction, NextCoords),
+    cell_value(Maze, NextCoords, Type),
+    Type \= w,
+    \+ member(NextCoords, Visited),
+    solve(Maze, NextCoords, [NextCoords | Visited], Rest).
+
+
+
+
--- devlog.md
+++ devlog.md
@@ -7,3 +7,6 @@ We are allowed to use recursion so I will probably be using it, and we are allow
 November 29, 2025:
 I have been sick all week, but did just finish HW 5. Today I want to try get my bearings back on the project and work on it tomorrow. 
 Today will just be me looking at my code again and trying to figure out what I did so far and what else I need to do.
+
+November 30, 2025:
+Today I want to try and get my move function made. his should just move my character to anoter move based on a direction, and maybe also something to solve the maze, whichc also checks if the move has already been made.
